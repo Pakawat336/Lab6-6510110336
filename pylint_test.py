e95021b35b1a36ee3f2e0a7d70876c879d1000bc
Pakawat Suksai <6510110336@psu.ac.th>
2023-12-14 13:46:42 +0700

    Fix pylint_test 1

--- pylint_test.py
+++ pylint_test.py
@@ -2,6 +2,9 @@ import os
 import subprocess
 
 def run_pylint():
+    """
+    Run Pylint on all Python files in the current directory.
+    """
     # Find all Python files in the current directory
     python_files = [file for file in os.listdir() if file.endswith('.py')]
 
@@ -11,7 +14,15 @@ def run_pylint():
 
     # Run Pylint on each Python file
     for python_file in python_files:
-        subprocess.run(['pylint', python_file])
+        # Construct the full path to the Python file
+        file_path = os.path.join(os.getcwd(), python_file)
+
+        # Run Pylint and capture the return code
+        result = subprocess.run(['pylint', file_path])
+
+        # Check the return code and handle it as needed
+        if result.returncode != 0:
+            print(f"Warning: Pylint returned a non-zero exit code for {python_file}.")
 
 if __name__ == "__main__":
     run_pylint()
